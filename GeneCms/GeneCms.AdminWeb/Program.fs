e7e88f393e2ab70e832b4d8a01817ae664243884
Juan Pablo <jptarqu@mesmerizingmemories.com>
2016-03-29 05:08:59 -0400

    Shell

--- GeneCms/GeneCms.AdminWeb/Program.fs
+++ GeneCms/GeneCms.AdminWeb/Program.fs
@@ -2,8 +2,22 @@
 // See the 'F# Tutorial' project for more help.
 
 open Suave
+open Suave.Operators
+
+let app =
+    choose
+        [ Filters.GET >=> choose
+
+            [ 
+//                Filters.path "/" >=> (
+//                  "My main page"
+//                  |> Successful.OK)
+
+              Files.browse @"C:\Users\jptar_000\Source\Repos\gene-cms\GeneCms\GeneCms.AdminWeb\bin\Debug\Generated"
+              ] 
+        ] // <-- The important part
 
 [<EntryPoint>]
-let main argv = 
-    startWebServer defaultConfig (Successful.OK "Hello World!")
-    0 // return an integer exit code
+let main x =
+    Web.startWebServer Web.defaultConfig app |> ignore
+    0
