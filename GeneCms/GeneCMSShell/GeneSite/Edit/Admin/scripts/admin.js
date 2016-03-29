f1ac78841872f9e320b3fc0da1c0f8c71c68dda5
Juan Tarquino <jtarquino@centerstatebank.com>
2016-03-29 15:43:13 -0400

    electron admin folder

---
+++ GeneCms/GeneCMSShell/GeneSite/Edit/Admin/scripts/admin.js
@@ -0,0 +1,12 @@
+var edge = require('edge');
+
+var helloFs = edge.func('fs', function () {/*
+    fun input -> async { 
+        return "F# welcomes " + input.ToString()
+    }
+*/});
+
+helloFs('Node.js', function (error, result) {
+    if (error) throw error;
+    console.log(result);
+});
